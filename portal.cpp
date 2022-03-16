8b10720049a919dd754cdc22062127fc160e8456
lob23 <Hvblong21@apcs.fitus.edu.vn>
2022-03-17 00:27:33 +0700

    menu

--- portal.cpp
+++ portal.cpp
@@ -1,9 +1,38 @@
 #include "display.h"
 #include "date.h"
+#include "string.h"
+#include "render_profile.h"
 
 using namespace std;
 
-int main() {
+int main(int argc, const char * argv[])
+{
+    //log in process
+    
+    //after log in successful
+    //print menu
+    int a;
+    bool is_staff;
+    if (is_staff==true) //if user is staff
+    {
+        cout<<"1. search for profile"<<endl;
+        cout<<"2. Set schoolyear"<<endl;
+        cout<<"3. Set semester"<<endl;
+        cout<<"4. Change password"<<endl;
+        cin>>a;
+        if (a==1)
+        {
+            render_staff_profile();
+        }
+        
+    }
+    if (is_staff==false) //if user is student
+    {
+        cout<<"..."<<endl;
+        cin>>a;
+    }
+    return 0;
+}
 
     return 0;
 }
