bbe46f7d112df159596b96461703e3ff84ada8ee
Huuhao1712 <95205995+Huuhao1712@users.noreply.github.com>
2022-03-18 16:40:32 +0700

    Create 4.h

---
+++ 4.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "course.h"
+#include "student.h"
+#include "clist.h"
+
+void viewCourses(student& student);
+void viewClasses();
+void viewStudentsinclass(unicalss& classname);
+void viewCourses(semester Semester);
+void viewStudentsinCourse(course course);
