a4b2a7b5a57214778727c72c5dea55f23bfabdb7
trietng <93473943+trietng@users.noreply.github.com>
2022-04-01 10:47:12 +0700

    New update to data structure

--- display.cpp
+++ display.cpp
@@ -1,15 +1,44 @@
 #include "display.h"
 
 void clrscr() {
-    #ifdef _WIN32
-        std::system("cls");
-    #else
-        // Assume POSIX
-        std::system("clear");
-    #endif
-}
+#ifdef _WIN32
+    HANDLE                     hStdOut;
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    DWORD                      count;
+    DWORD                      cellCount;
+    COORD                      homeCoords = { 0, 0 };
 
+    hStdOut = GetStdHandle( STD_OUTPUT_HANDLE );
+    if (hStdOut == INVALID_HANDLE_VALUE) return;
+
+    /* Get the number of cells in the current buffer */
+    if (!GetConsoleScreenBufferInfo( hStdOut, &csbi )) return;
+    cellCount = csbi.dwSize.X *csbi.dwSize.Y;
+
+    /* Fill the entire buffer with spaces */
+    if (!FillConsoleOutputCharacter(
+        hStdOut,
+        (TCHAR) ' ',
+        cellCount,
+        homeCoords,
+        &count
+    )) return;
+
+    /* Fill the entire buffer with the current colors and attributes */
+    if (!FillConsoleOutputAttribute(
+        hStdOut,
+        csbi.wAttributes,
+        cellCount,
+        homeCoords,
+        &count
+    )) return;
+
+    /* Move the cursor home */
+    SetConsoleCursorPosition( hStdOut, homeCoords );
+#else
+    
+#endif
+}
 void thread_sleep(const unsigned int& time) {
     std::this_thread::sleep_for(std::chrono::milliseconds(time));
 }
-
--- display.h
+++ display.h
@@ -1,7 +1,11 @@
 #pragma once
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <unistd.h>
 #include <cstdlib>
 #include <chrono>
 #include <thread>
+#include <curses.h>
 
 void clrscr();
 void thread_sleep(const unsigned int& time);
